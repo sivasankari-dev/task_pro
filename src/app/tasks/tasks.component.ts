f2b0620f5d444058c679e16584074c9ab73d106b
sivasankari-dev <sivasankari.devofficial@gmail.com>
2025-02-27 23:21:01 +0530

    Adding tabs

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_TASKS } from '../dummy-tasks';
-//import { type Tasks } from './task/task.model';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTaskdata } from './task/task.model';
 import { TasksService } from './task.services';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent,NewTaskComponent],
+  imports: [TaskComponent,NewTaskComponent,CommonModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -25,10 +23,29 @@ export class TasksComponent {
   }
 
   isAddingTasks = false;
+  selectedTab: string = 'open';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user_id']) {
+      this.selectedTab = 'open';
+    }
+  }
 
   get selectedUserTasks () {
 
     return this.tasksService.getUserTasks(this.user_id) || [];
+  }
+
+  getOpenTasks(): any[] {
+    return this.selectedUserTasks.filter(task => !task.isCompleted);
+  }
+
+  getcompletedTasks(): any[] {
+    return this.selectedUserTasks.filter(task => task.isCompleted);
+  }
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
   }
 
   onStartAddTask() {
